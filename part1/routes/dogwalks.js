43e6645e6b460a3dfcd98d511e4814fd87f96c27
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 04:00:13 +0000

    [automatic commit] 2025-06-20 04:00:13 UTC 175.35.108.85

--- part1/routes/dogwalks.js
+++ part1/routes/dogwalks.js
@@ -19,7 +19,7 @@ router.get('/walkrequests/open', async (req, res) => {
     try {
  const [rows] = await db.query(`
 SELECT wr.request_id, d.name, wr.requested_time, wr.location, u.username
-FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = 
+FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id
     `);
 res.json(rows);
     }catch(error){
