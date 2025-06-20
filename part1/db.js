8a6bf41449479967f2e709e51b409e5c08eae812
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 03:33:18 +0000

    [automatic commit] 2025-06-20 03:33:18 UTC 175.35.108.85

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: '127',
+    host: '127.0.0.1',
     user: 'root',
     password: 'password',
     database: 'DogWalkService'
