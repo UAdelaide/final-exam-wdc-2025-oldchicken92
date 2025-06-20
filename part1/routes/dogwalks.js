7bd91bde09948038a029a3f4da2aea24158a09d3
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 04:26:34 +0000

    [automatic commit] 2025-06-20 04:26:34 UTC 175.35.108.85

--- part1/routes/dogwalks.js
+++ part1/routes/dogwalks.js
@@ -34,7 +34,7 @@ SELECT u.username,
 COUNT(wr.rating) AS total_ratings,
 AVG(wr.rating) AS average_rating,
 COUNT(wa.request_id) AS completed_walks FROM Users u
-JOIN WalkRatings wr ON wa.request_id = wr.request_id AND wa.walker
+JOIN WalkRatings wr ON wa.request_id = wr.request_id AND wa.walker_id = wr.walker_id
 JOIN
 JOIN
 WHERE u.role = 'walker'
