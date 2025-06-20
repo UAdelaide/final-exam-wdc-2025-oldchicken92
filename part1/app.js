93a65cdc85798294b540bf532df58611c157c8ce
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 03:22:12 +0000

    [automatic commit] 2025-06-20 03:22:12 UTC 175.35.108.85

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogwalksRouter = require('./')
+var dogwalksRouter = require('./routes/dogwalks');
 
 var app = express();
 
@@ -17,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api')
 
 module.exports = app;
