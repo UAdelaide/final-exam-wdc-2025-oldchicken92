75ae4c0ed957dd4ec82005a4db2921e7b320da5e
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 03:53:53 +0000

    [automatic commit] 2025-06-20 03:53:53 UTC 175.35.108.85

--- part1/routes/dogwalks.js
+++ part1/routes/dogwalks.js
@@ -18,7 +18,7 @@ res.status(500).send('Dogs database error');
 router.get('/walkrequests/open', async (req, res) => {
     try {
  const [rows] = await db.query(`
-
+SELECT 
     `);
 res.json(rows);
     }catch(error){
