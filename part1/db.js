446a83260c06ab27ac73fc53099bcbef663b6ab4
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 03:39:39 +0000

    [automatic commit] 2025-06-20 03:39:39 UTC 175.35.108.85

--- part1/db.js
+++ part1/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
+    socketPath: '/var/run/mysqld/mysqld.sock',
     host: '127.0.0.1',
     user: 'root',
     password: 'newpassword',
