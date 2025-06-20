35496fb7226d6aa51f8e5808d271a97d65050811
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 04:09:41 +0000

    [automatic commit] 2025-06-20 04:09:41 UTC 175.35.108.85

--- part1/routes/dogwalks.js
+++ part1/routes/dogwalks.js
@@ -32,7 +32,7 @@ router.get('/walkers/summary', async (req, res) => {
  const [rows] = await db.query(`
 SELECT u.username, COUNT(wr.rating) AS total_ratings,
 AVG(wr.rating) AS average_rating,
-
+COUNT(wa.request_id) AS 
 
     `);
 res.json(rows);
