9169594d50d54ba0b906a89033e8799220fea2ca
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-20 03:23:29 +0000

    [automatic commit] 2025-06-20 03:23:29 UTC 175.35.108.85

--- part1/routes/dogwalks.js
+++ part1/routes/dogwalks.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/dogs', async (req))
+router.get('/dogs', async (req, res) => {
+
+
+});
 
 
 
