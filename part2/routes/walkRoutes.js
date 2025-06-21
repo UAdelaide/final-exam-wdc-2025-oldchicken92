1a1ec4f07d4eef308fa4b030e5854f93b83a99e2
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-21 05:29:29 +0000

    [automatic commit] 2025-06-21 05:29:29 UTC 14.201.62.94

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -64,7 +64,7 @@ router.get('/user-dogs', async (req, res) => {
   const user = req.session.user;
 
   const [dogs] = await db.query(`
-    SELECT
+    SELECT dog_id, name FROM Dogs WHERE owner
     `)
 
 });
