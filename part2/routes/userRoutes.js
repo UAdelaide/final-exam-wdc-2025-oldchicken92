d70cd52979b663bf4068b32ba664417b60a24290
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-21 02:45:54 +0000

    [automatic commit] 2025-06-21 02:45:54 UTC 14.201.62.94

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,6 +35,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
+
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
