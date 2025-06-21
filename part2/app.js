c299f95fd8dc3a02647cc2c63824984760e32db4
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-21 02:50:27 +0000

    [automatic commit] 2025-06-21 02:50:27 UTC 14.201.62.94

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// session data config
+
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
