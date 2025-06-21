4da3c9bb3c40fc0417d92ff2fa53577c2bba7cd3
oldchicken92 <a1926213@adelaide.edu.au>
2025-06-21 04:35:45 +0000

    [automatic commit] 2025-06-21 04:35:45 UTC 14.201.62.94

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -80,9 +80,7 @@ router.post('/login', async (req, res) => {
 // logout method
 router.post('/logout', (req, res) => {
   // remove all session data
-req.session.destroy((err) => {
-
-});
+req.session.destroy
 
 
 });
